790fd8824cbbf2679e118d91f34933816e42e72e
Nicolas Fecko <141719332+NicolasFecko@users.noreply.github.com>
2026-07-07 12:57:03 +0200

    Added a code file with Hello World
    The file showcases 2 types of a Hello World program

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+// This is a compact version of Hello World in Java
+void main (String[] args) {
+    System.out.println("Hallo Welt!");
+}
+
+// This is the longer version
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
